3cd9c84f299679648fde4d7614a35d9a993fd484
Abhii <157186373+abvyyii@users.noreply.github.com>
2024-11-07 18:22:46 +0545

    Update insert.php

--- insert.php
+++ insert.php
@@ -12,10 +12,10 @@ if(!empty($firstname) || !empty($lastname) || !empty($dob) || !empty($email) ||!
     $dbPassword="";
     $database_name="form";
 
-    $conn = mysqli_connect($host, $dbUsername, $dbPassword, $dbname);
+    $conn = mysqli_connect($host, $dbUsername, $dbPassword, $database_name);
 
     if(mysqli_connect_error()){
-        die("Connection error: ". mysqli_coonect_error());
+        die("Connection error: ". mysqli_connect_error());
     } else{
         $SELECT = "SELECT email From table1 where email= ? Limit 1";
         $INSERT = "INSERT into table1 (firstname, lastname, dob, email, gender, phno) values(?,?,?,?,?,?)";
@@ -31,7 +31,7 @@ if(!empty($firstname) || !empty($lastname) || !empty($dob) || !empty($email) ||!
             $stmt->close();
 
             $stmt = $conn->prepare($INSERT);
-            $stmt->bind_param("sssssi", $firstname, $lastname, $dob, $gender, $phno);
+            $stmt->bind_param("sssssi", $firstname, $lastname, $dob, $gender, $email, $phno);
             $stmt->execute();
             echo "New record inserted sucessfully";
 
